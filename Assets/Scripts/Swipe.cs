f2a47a5ff043cd6123ad225b910093a1839e1dcb
Marcin <lewandowski_marcin@hotmail.com>
2017-10-29 18:10:26 +0100

    adding new project

---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetTouch(0).deltaPosition.x > 0)
+        {
+            Debug.Log("right"); 
+        }
+        if (Input.GetTouch(0).deltaPosition.x < 0)
+        {
+            Debug.Log("left"); 
+        }
+        if (Input.GetTouch(0).deltaPosition.y < 0)
+        {
+            Debug.Log("down"); 
+        }
+        if (Input.GetTouch(0).deltaPosition.y > 0)
+        {
+            Debug.Log("up"); 
+        }
+
+    }
+}
